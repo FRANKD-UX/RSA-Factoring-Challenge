e827469a407fd9dac8c050f0ed1ad3528595a2f2
FRANKD-UX <frankdafrica@gmail.com>
2024-09-26 20:12:39 -0700

    f

--- factors.c
+++ factors.c
@@ -1,64 +1,42 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <math.h>
 #include "main.h"
 
 /**
- * factorize - Factorize a number into a product of two smaller numbers
- * @n: The number to factorize
- *
- * Description: This function takes a natural number and outputs the product
- * of two smaller numbers (p and q) such that n = p * q.
+ * factorize - Finds and prints the factorization of a number
+ *             in the format n=p*q where p > q.
+ * @n: The number to be factorized.
  */
 void factorize(unsigned long n)
 {
 	unsigned long p, q;
+	unsigned long i;
 
-	for (p = 2; p <= n / 2; p++)
+	/* Loop through numbers from 2 to sqrt(n) */
+	for (i = 2; i * i <= n; i++)
 	{
-		if (n % p == 0)
+		/* If i is a factor of n */
+		if (n % i == 0)
 		{
-			q = n / p;
-			printf("%lu=%lu*%lu\n", n, q, p);
+			q = i;
+			p = n / i;
+
+			/* Ensure the larger factor is printed first */
+			if (p < q)
+			{
+				unsigned long temp = p;
+
+				p = q;
+				q = temp;
+			}
+
+			/* Print the factorization */
+			printf("%lu=%lu*%lu\n", n, p, q);
 			return;
 		}
 	}
+
+	/* If no factors were found, n is prime */
 	printf("%lu=%lu*1\n", n, n);
-}
-
-/**
- * main - Entry point of the program
- * @argc: Argument count
- * @argv: Argument vector
- *
- * Description: The main function reads a file containing natural numbers and
- * factorizes each number using the factorize function.
- *
- * Return: 0 on success, or 1 if an error occurs
- */
-int main(int argc, char *argv[])
-{
-	FILE *file;
-	unsigned long num;
-
-	if (argc != 2)
-	{
-		fprintf(stderr, "Usage: %s <file>\n", argv[0]);
-		return (1);
-	}
-
-	file = fopen(argv[1], "r");
-	if (file == NULL)
-	{
-		fprintf(stderr, "Error: Could not open file %s\n", argv[1]);
-		return (1);
-	}
-
-	while (fscanf(file, "%lu", &num) != EOF)
-	{
-		factorize(num);
-	}
-
-	fclose(file);
-	return (0);
 }
 
