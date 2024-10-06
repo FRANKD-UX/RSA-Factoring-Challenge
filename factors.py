581b675d6ddbb24bd8fd7dc05c81c4ac7a24b945
FRANKD-UX <frankdafrica@gmail.com>
2024-10-05 19:16:41 -0700

    f

---
+++ factors.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import sys
+
+
+def factorize(n):
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return i, n // i
+    return n, 1
+
+
+def main(file_path):
+    with open(file_path, 'r') as file:
+        numbers = file.readlines()
+
+    for number in numbers:
+        n = int(number.strip())
+        p, q = factorize(n)
+        print(f"{n}={p}*{q}")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: factors <file>")
+        sys.exit(1)
+
+    file_path = sys.argv[1]
+    main(file_path)
--- main.h
+++
@@ -1,6 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-void factorize(unsigned long n);
-
-#endif /* MAIN_H */
