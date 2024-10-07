cb20947342323f12967244071329ac1b511a14c8
FRANKD-UX <frankdafrica@gmail.com>
2024-10-06 18:59:28 -0700

    f

--- factors.c
+++ factors.c
@@ -1,73 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include "main.h"
 
-/**
- * find_factors - Finds two factors of a number n.
- * This function prints the factors in the format n = p * q
- * where p and q are factors of n.
- * @n: The number to factorize.
- *
- * Return: void
- */
-void find_factors(unsigned long n)
+int main(void)
 {
-	unsigned long i;
+	long long int num = 239809320265259;
+	long int factor1 = 2;
+	long int factor2;
 
-	/* Try to find two factors by iterating from 2 up to sqrt(n) */
-	for (i = 2; i <= sqrt(n); i++)
+	while (num % factor1)
 	{
-		if (n % i == 0)
+		if (factor1 <= num)
 		{
-			/* We found two factors: i and n / i */
-			printf("%lu=%lu*%lu\n", n, i, n / i);
-			return;
+			factor1++;
+		}
+		else
+		{
+			return (-1);
 		}
 	}
-	/* If no factors found, n is prime and can be factorized as 1*n */
-	printf("%lu=%lu*1\n", n, n);
-}
 
-/**
- * main - Reads the input file and factorizes the numbers.
- * @argc: Argument count.
- * @argv: Argument vector (file path).
- *
- * Return: 0 on success, 1 on error.
- */
-int main(int argc, char *argv[])
-{
-	FILE *file;
-	char *line = NULL;
-	size_t len = 0;
-	unsigned long number;
-
-	/* Check if the file argument is provided */
-	if (argc != 2)
-	{
-		fprintf(stderr, "Usage: %s <file>\n", argv[0]);
-		return (1);
-	}
-
-	/* Open the file for reading */
-	file = fopen(argv[1], "r");
-	if (file == NULL)
-	{
-		perror("Error opening file");
-		return (1);
-	}
-
-	/* Read each line of the file, convert it to an integer, and factorize it */
-	while (getline(&line, &len, file) != -1)
-	{
-		number = strtoul(line, NULL, 10);
-		find_factors(number);
-	}
-
-	/* Cleanup */
-	free(line);
-	fclose(file);
-
+	factor2 = num / factor1;
+	printf("%lld = %ld * %ld\n", num, factor2, factor1);
 	return (0);
 }
+
